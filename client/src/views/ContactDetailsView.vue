d439fb56cdf27e2abfe8fac139b89c3464856c3b
Paulo Cuambe <paulo.cuambe@rgsistemas.com.br>
2022-04-06 08:31:27 +0200

    client: cleanup

---
+++ client/src/views/ContactDetailsView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const router = useRoute();
+const id = ref(router.params.id);
+</script>
+<template>
+  <h1>Ohhh, my details {{ id }}</h1>
+</template>
