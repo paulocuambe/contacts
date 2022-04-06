1a1c0842390f93199f9d6c1c031a3b37f36533e3
Paulo Cuambe <paulo.cuambe@rgsistemas.com.br>
2022-04-06 09:52:04 +0200

    client: add proxy to prevent cors errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
+  },
 });
