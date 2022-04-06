2ae2c31ab7c046fd69864da6cf317c56bbeb9870
Paulo Cuambe <paulo.cuambe@rgsistemas.com.br>
2022-04-06 10:10:39 +0200

    client: add loading logic to home page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,15 +4,28 @@ import { onBeforeMount } from "@vue/runtime-core";
 import { ref } from "vue";
 
 const contacts = ref([]);
+const loadingState = ref("loading");
+const error = ref({});
+
 onBeforeMount(async () => {
   let request = await fetch("/api/contacts");
-  contacts.value = await request.json();
+  if (request.status != 200) {
+    error.value = await request.json();
+    loadingState.value = "error";
+  } else {
+    contacts.value = await request.json();
+    loadingState.value = "success";
+  }
 });
 </script>
 
 <template>
   <main>
-    <h1>Hello there</h1>
-    <contact-item-list :contacts-list="contacts"></contact-item-list>
+    <div v-if="loadingState === 'loading'">Loading contacts...</div>
+    <div v-else-if="loadingState === 'error'">Error: {{ error.message }}</div>
+    <div v-else-if="contacts.length === 0">
+      <h2>You have not added any contacts yet. Click here to get started</h2>
+    </div>
+    <contact-item-list v-else :contacts-list="contacts"></contact-item-list>
   </main>
 </template>
